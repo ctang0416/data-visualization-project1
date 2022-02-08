099b9cb4865b50aa47d68c2c4ae2964eb0c7fd8a
Christine Tang <christine1253@gmail.com>
2022-02-08 02:39:23 -0800

    finish project

--- src/App.jsx
+++ src/App.jsx
@@ -72,7 +72,7 @@ function App() {
           />
 
           <text x="-400" y="30" transform="rotate(-90)" fontSize={16}>
-            100,000 people
+            Population (in 100000 people)
           </text>
 
           <text x="600" y="720" fontSize={16}>
@@ -105,12 +105,53 @@ function App() {
             );
           })}
 
+<rect
+            x={chartSizeWidth - 135}
+            y={50}
+            height={20}
+            width={30}
+            fill={`rgb(${200},${200},${200})`}
+          />
+
+          <rect
+            x={chartSizeWidth - 135}
+            y={20}
+            height={20}
+            width={30}
+            fill={`rgb(${100},${100},${100})`}
+          />
+          
+
+          <text x={chartSizeWidth -100} y="35" fontSize={12}>
+            1900
+          </text>
+
+          <text x={chartSizeWidth -100} y="65" fontSize={12}>
+            2000
+          </text>
+
         </svg>
       </div>
-      <text>
-            Data Description:
-            In the year 1900 and 2000 
-      </text>
+      <p>
+      Since I would like to compare the population in 1900 and 2000 in the chart, I choose to use a grouped bar 
+      chart to show the data. By having the bars of the same age in 1900 and 2000 next to each other,  it would 
+      be easier to visually compare the data and can clearly see the differences between 1900 and 2000. For the 
+      color shades of the bar, I choose to use just a darker grey and a lighter grey instead of using different 
+      colors because I think this way would make the visualization cleaner. For the y-axis scale, I make the unit 
+      be in 100000 people so that the scale won’t be a long number which might be hard to read.
+      </p>
+      <p>
+      One of the insights I got from the chart is that there is a huge population growth between 1900 and 2000. I 
+      think one of the reasons might be a baby boom. There was a baby boom from 1946  to 1964. As we can see in the 
+      cart, the ages that have the most significant difference between 1900 and 200 are age 35 and 40, which those 
+      people’s age match the year of the baby boom. Another reason that might increase the population might be 
+      immigration. There are some immigration waves between 1900 and 2000. With immigration, it can increase the 
+      population at any age. Additionally, in the chart, we can also see that there are more people at the age of 
+      90 in 2000 than in 1900. With all the technology and medication improvement, I think people would be able to 
+      live a longer life. There are also a lot more babies born in 2000 compared to 1900. Since there are more people 
+      in the age of 20s and 30s, it’s reasonable that there are more babies.
+      </p>
+      <p> Link to Github page: <a href="https://github.com/ctang0416/data-visualization-project1.git">https://github.com/ctang0416/data-visualization-project1.git</a></p>
     </div>
   );
 }
